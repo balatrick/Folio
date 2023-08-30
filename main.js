ca9ab9784d9cbc18bdfcd123d158cffe94759369
balatrick <balatrick>
2023-08-30 22:35:14 +0530

    deploy

---
+++ main.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".text", {
+    strings: ['<i>Frontend developer</i>', '&amp; <i>web developer.</i>'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+});
+
+
+
+console.log("hi bala")
+
+
