c475c4aee7ed3e53b24969e0c0c9799bc1414d6b
Balasubramaniyan <86256242+balatrick@users.noreply.github.com>
2023-09-14 10:49:14 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,19 @@ var typed = new Typed(".text", {
     loop: true,
 });
 
+const dark = document.getElementById("dark");
 
+console.log("dark")
+
+window.onmousemove = function(e){
+    const x = e.x / window.innerWidth * 100;
+    const y = e.y / window.innerHeight * 100;
+    dark.style.background = `radial-gradient(
+        circle at ${x}%  ${y}%,
+        transparent, rgba(0,0, 0, .9) 150px, black
+    )`
+    console.log(y)
+}
 
 console.log("hi bala")
 
